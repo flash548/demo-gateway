747353fa72aa1127fb073c49c0cf30f7cb87070e
flash548 <christopher.zell@gmail.com>
2024-09-02 11:13:45 -0400

    props routes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,24 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.function.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.stripPrefix;
-import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 
     /**
      * Used for our custom logout handler
@@ -33,37 +26,4 @@ public class DemoApplication {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
-
-
-    /**
-     * For demo's sake, poor man's route config, and setup.  Atrociously hardcoded.
-     * @return
-     */
-    @Bean
-    public RouterFunction<ServerResponse> getRoute() {
-        Map<String, String> routeMap = new HashMap<>();
-        routeMap.put("/api/catalog/**", "http://localhost:8082");
-        routeMap.put("/api/user-mgmt/**", "http://localhost:8083");
-
-        var router = route();
-        for (var kvp : routeMap.entrySet()) {
-            router.add(
-                    route().route(RequestPredicates.path(kvp.getKey()), http(kvp.getValue()))
-                            .filter(stripPrefix(2))
-                            .build());
-        }
-        return router.build();
-    }
-
-    private static HandlerFilterFunction<ServerResponse, ServerResponse> erasePathPrefix(String prefix) {
-        return (request, next) -> {
-            if (prefix == null) return next.handle(request);
-
-            String newPath = request.uri().getPath().replaceAll("^" + prefix, "");
-            URI u = UriComponentsBuilder.fromPath(newPath).build().toUri();
-            ServerRequest modified = ServerRequest.from(request).uri(u).build();
-            return next.handle(modified);
-        };
-    }
-
 }
